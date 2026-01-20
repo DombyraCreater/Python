9568720b48284a073c0dd44d869614596a31b4fa
Elaman <azimbayelaman@gmail.com>
2026-01-20 16:48:03 +0500

    Vars

---
+++ VarNames.py
@@ -0,0 +1,21 @@
+"""
+legal var names
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John
+"""
+"""
+illegal var names
+2myvar = "John"
+my-var = "John"
+my var = "John"
+"""
+#Camel Case
+myVariableName = "John"
+#Pascal Case
+MyVariableName = "John"
+#Snake Case
+my_variable_name = "John"
