b7a4ffc6f6849b0732dec143b3bd703ba9880995
Elaman <azimbayelaman@gmail.com>
2026-01-20 16:57:06 +0500

    outputs

---
+++ Output vars.py
@@ -0,0 +1,28 @@
+x = "Python is awesome"
+print(x)
+"""
+In the print() function, you output multiple variables, separated by a comma:
+"""
+x = "Python"
+y = "is"
+z = "awesome"
+print(x, y, z)
+#You can also use the + operator to output multiple variables:
+x = "Python "
+y = "is "
+z = "awesome"
+print(x + y + z)
+#For numbers, the + character works as a mathematical operator:
+x = 5
+y = 10
+print(x + y)
+#U cant
+"""
+x = 5
+y = "John"
+print(x + y)
+"""
+#instead
+x = 5
+y = "John"
+print(x, y)
